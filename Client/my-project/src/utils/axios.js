30a5f5798c555b6bc9954b5cf35cfa64064e19c1
Soorajhari <soorajhari1999@gmail.com>
2024-01-19 23:50:31 +0530

    message

--- Client/my-project/src/utils/axios.js
+++ Client/my-project/src/utils/axios.js
@@ -2,9 +2,7 @@ import axios from "axios";
 const instance = axios.create({
   baseURL: "https://product-managment-server.vercel.app/",
 
-  headers: {
-    "Content-Type": "application/json",
-  },
+
 });
 
 export default instance;
